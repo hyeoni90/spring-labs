e1d88a933433f35353e06d26f41ef53198ad0df2
hyeoni90 <gusk743@gmail.com>
2021-04-13 23:36:38 +0900

    spring boot jwt initial commit

---
+++ spring-boot-jwt/src/main/java/com/hyeonah/springbootjwt/SpringBootJwtApplication.java
@@ -0,0 +1,13 @@
+package com.hyeonah.springbootjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtApplication.class, args);
+    }
+
+}
