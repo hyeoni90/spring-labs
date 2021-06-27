b453d3fdc99065112e44532701e3ca5826755da3
hyeoni90 <gusk743@gmail.com>
2021-06-27 20:45:20 +0900

    initial commit

---
+++ spring-cloud-openfeign/README.md
@@ -0,0 +1,4 @@
+# Spring Cloud OpenFeign Example
+
+## References
+* [Spring Cloud OpenFeign](https://cloud.spring.io/spring-cloud-openfeign/reference/html/)
---
+++ spring-cloud-openfeign/src/main/java/com/hyeonah/springcloudopenfeign/SpringCloudOpenfeignApplication.java
@@ -0,0 +1,13 @@
+package com.hyeonah.springcloudopenfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudOpenfeignApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(SpringCloudOpenfeignApplication.class, args);
+    }
+
+}
