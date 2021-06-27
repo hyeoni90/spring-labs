47ba31ead708addbeaee711bed3266c1bce1a94c
hyeoni90 <gusk743@gmail.com>
2021-06-27 20:46:54 +0900

    feat: Add openfeign config

--- spring-cloud-openfeign/src/main/java/com/hyeonah/springcloudopenfeign/SpringCloudOpenfeignApplication.java
+++ spring-cloud-openfeign/src/main/java/com/hyeonah/springcloudopenfeign/SpringCloudOpenfeignApplication.java
@@ -2,8 +2,10 @@ package com.hyeonah.springcloudopenfeign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SpringCloudOpenfeignApplication {
 
     public static void main(final String[] args) {
